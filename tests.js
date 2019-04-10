daa098d9b4c9cc76a3ebf9bb30485d7622cdd5f3
Hillary Spitzer <hspitzer2@gmail.com>
2019-04-10 00:40:25 +0000

    Automatically backed up by Learn

--- tests.js
+++ tests.js
@@ -1,6 +1,6 @@
 var index = require("./index.js")
 if (name ==="joe"){
-  console.log( "the name is Correct")
+  console.log( "the name is correct")
 }
 if (height === 70) {
     console.log("The height is correct")
