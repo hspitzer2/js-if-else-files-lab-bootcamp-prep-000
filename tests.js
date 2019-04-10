04d458c1ebc7f5b3f2a8ffe16244b2755e5c4b6c
Hillary Spitzer <hspitzer2@gmail.com>
2019-04-10 00:28:04 +0000

    Automatically backed up by Learn

--- tests.js
+++ tests.js
@@ -12,4 +12,33 @@ if (index.height === 70) {
     console.log("The height is correct")
 } else {
     console.log("Expected: 70, Received: "+index.height)
+}if (name==="Expected: Susan, Received: Joe"){
+  console.log("The name is correct")
+}if (height==="Expected: 70, Received: 74"){
+  console.log("The height is correct")
+}
+var name = "Joe"
+var height = "74"
+ 
+// Don't worry about this
+module.exports = { name, height
+}
+if (name === "Susan") {
+    console.log("The name is correct")
+} else {
+    console.log("Expected the name to be Susan")
+}
+var name = "Susan"
+var name = "Joe"
+var height = "74"
+if (name === "Expected: Susan, Received: Joe") {
+    console.log("The name is correct")
+} else {
+    console.log("Expected the name to be Susan")
+}if (height==="Expected: 70, Received: 74"){
+  console.log("The height is correct")
+}
+ 
+// Don't worry about this
+module.exports = { name, height
 }
