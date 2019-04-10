c876ae874f0196bc0d3f7f990e4e3bff9b16cd1d
Hillary Spitzer <hspitzer2@gmail.com>
2019-04-10 00:35:37 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 var name = "Joe"
 var height = "74"
- 
+if (name === "Joe"){
+console.log("This name is correct")
+  
+}if (height === "70"){
+  console.log ("This height is correct")
+}
 // Don't worry about this
 module.exports = { name, height
 }
